b3474e0599f57ebd793424c3b205ee513de640f5
walterblack6 <124960567+walterblack6@users.noreply.github.com>
2025-06-24 15:09:25 +1000

    Change movement keys to WASD
    Replaced arrow key controls with WASD

--- Adventure_game/main.py
+++ Adventure_game/main.py
@@ -25,13 +25,13 @@ while running:
 
     # Movement
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_a]:
         player_rect.x -= speed
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_d]:
         player_rect.x += speed
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_w]:
         player_rect.y -= speed
-    if keys[pygame.K_DOWN]:
+    if keys[pygame.K_s]:
         player_rect.y += speed
 
     # Draw player
