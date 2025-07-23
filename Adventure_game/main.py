43f3b9aece84e77736c0778b00e350b3e0b3de8f
walterblack6 <124960567+walterblack6@users.noreply.github.com>
2025-07-23 12:55:42 +1000

    add npc and textbox

--- Adventure_game/main.py
+++ Adventure_game/main.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 from player import Player  # import player class
 from map import tile_map, draw_map, get_tile
-
+from npc import NPC
 # Initialize Pygame
 pygame.init()
 
@@ -11,8 +11,18 @@ WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Adventure Quest")
 
+def draw_textbox(screen, text):
+    font = pygame.font.SysFont("Arial", 20)
+    pygame.draw.rect(screen, (0, 0, 0), (50, 500, 700, 80)) #draw textbox background
+    rendered = font.render(text, True, (255, 255, 255)) #render text
+    screen.pygame.Surface.blit(rendered, (60, 530)) #draw text in box
+
 # Set up player
 player = Player(375, 275)  # create player instance
+
+npc1 = NPC(300, 200, "SHANE RILE TAN") # create npc instance
+
+dialogue_message = "" # store message to display
 
 # Game loop
 running = True
@@ -32,6 +42,20 @@ while running:
 
     # Draw player
     player.draw(screen)  # use player's draw method
+    
+    # Draw npc
+    npc1.draw(screen)
+    
+    #npc interaction
+    if keys[pygame.K_SPACE]:
+        message = npc1.interact(player.rect)
+        if message:
+            dialogue_message = message  # Set message to display
+            
+    
+    # Draw textbox if there's a message
+    if dialogue_message:
+        draw_textbox(screen, dialogue_message)
 
     # Check tile under player
     tile_under_player = get_tile(player.rect.centerx, player.rect.centery)
